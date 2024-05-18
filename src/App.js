689512527a504807f54ceb9fc649776915aaa7b0
tkocz <tkocz@d17-428-pc-05.ipa.ii.agh.edu.pl>
2024-05-18 11:14:56 +0200

    Next changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
+import {useState} from "react";
+
 function App() {
-  let email = 'ola@ola.ola';
-  function handleChange(event) {
 
-    console.log(event.target.value);
+  const [email, setEmail] = useState('example_email@example.com');
+  let info;
 
-  }
+    if (email.length<5) {
+        info = <div>Twój email jest za krótki</div>;
+    } else if (email.length < 40) {
+        info = <div>Twój adres e-mail jest w sam raz</div>;
+    } else {
+        info = <div>Twój adres e-mail jest stanowczo za długi</div>;
+    }
+    function handleChange(event) {
+        setEmail(event.target.value);
+    }
+
   return (
 
       <div>
         <h1>System do zapisów na zajęcia</h1>
         <h2>Twój e-mail to {email}</h2>
-        <input type="text" onChange={handleChange}/>
+        {info}
+        <input type="text" value={email} onChange={handleChange}/>
       </div>
 
   );
